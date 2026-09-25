42ab5b45cde6a49ff1094435fa8254aa41532151
mapi-developer <matveypisarev.edu@gmail.com>
2026-09-25 12:16:43 +0200

    upd: variables testing

--- seminar_second/FormMain.cs
+++ seminar_second/FormMain.cs
@@ -10,12 +10,22 @@ namespace seminar_second
         private void Form_Load(object sender, EventArgs e)
         {
             byte b = 255;
+
             short s = -16;
             ushort us = 43000;
+
             int i = -32652;
             uint ui = 1234;
             long l = -12121212121212;
             ulong ul = 8932179843689;
+
+            double d = 3.14;
+            float f = 9.8f;
+            decimal dec = 3.04m;
+
+            bool isItString = false;
+            string str = "La Papoi";
+            char ch = 'c';
         }
     }
 }
