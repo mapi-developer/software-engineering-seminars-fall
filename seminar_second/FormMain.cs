2f538eeda7c7c29590d70d36f74d03f61749b07d
mapi-developer <matveypisarev.edu@gmail.com>
2026-09-25 12:11:44 +0200

    add: second_seminar_project added

---
+++ seminar_second/FormMain.cs
@@ -0,0 +1,21 @@
+namespace seminar_second
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            byte b = 255;
+            short s = -16;
+            ushort us = 43000;
+            int i = -32652;
+            uint ui = 1234;
+            long l = -12121212121212;
+            ulong ul = 8932179843689;
+        }
+    }
+}
